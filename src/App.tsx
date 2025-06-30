387d488fe99e8d9cd0b2e9b87e5125553540b2e2
mahesh555550 <uppalapatimahesh3@gmail.com>
2025-06-30 07:32:06 -0500

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { supabase } from './lib/supabase';
+import  { useState, useEffect } from 'react';
+//import { supabase } from './lib/supabase';
 import { useAuth } from './context/AuthContext';
 import { useSubscription } from './context/SubscriptionContext';
 import LandingPage from './components/LandingPage';
