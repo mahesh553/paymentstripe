4198a85f91c8e208d3f8dbdf977fd878bae23ce2
mahesh555550 <uppalapatimahesh3@gmail.com>
2025-07-02 21:24:35 -0500

    Updated JobMatchValidator.tsx

--- src/components/JobMatchValidator.tsx
+++ src/components/JobMatchValidator.tsx
@@ -75,7 +75,8 @@ const [isGenerating, setIsGenerating] = useState(false);
       const resume = JSON.parse(resumeData);
           const missingKeywords = matchResults.keyword_analysis?.missing_keywords || [];
 
-    const jd=jobDescription.substring(0, 300));
+          const jd = jobDescription.substring(0, 2000);
+
       console.log('Missing Keywords:', missingKeywords);
      console.log('Matching Skills:', matchResults.matching_skills || []);
    console.log('Job Description--:',jd);
